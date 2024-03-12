4d371cbb69e13f78e85fea9bd959037b9a503642
MarcosBoscariolo <marcosboscariolo@hotmail.com>
2024-03-12 13:45:17 -0300

    feat: header da aplicacao

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule
+  ],
+  exports: [
+    HeaderComponent
   ]
 })
 export class CoreModule { }
